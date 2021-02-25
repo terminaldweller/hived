39b361d24df4455c96b6ce952aec3bf9844b70ec
terminaldweller <thabogre@gmail.com>
2021-02-25 12:42:41 +0330

    turns out we really need that health endpoint after all. travis fix...

--- test/endpoints.sh
+++ test/endpoints.sh
@@ -2,6 +2,8 @@
 set -e
 set -x
 
+sleep 5
 curl -X GET http://127.0.0.1:8008/price?name=CAKE&unit=USD
 curl -X GET http://127.0.0.1:8008/pair?one=ETH&two=CAKE&multiplier=4.0
 curl -X POST -H "Content-Type: application/json" -d '{"name":"alert1", "expr":"ETH>CAKE"}' http://127.0.0.1:8008/addalert
+#curl -X POST 
