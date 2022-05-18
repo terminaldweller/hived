488d609a64d35683a824bef60841665942bf5cf9
terminaldweller <thabogre@gmail.com>
2022-05-18 19:13:38 +0430

    update

--- test/endpoints.sh
+++ test/endpoints.sh
@@ -6,3 +6,7 @@ set -x
 curl -k -X GET https://localhost:8008/crypto/price?name=CAKE&unit=USD
 curl -k -X GET https://localhost:8008/crypto/pair?one=ETH&two=CAKE&multiplier=4.0
 curl -k -X POST -H "Content-Type: application/json" -d '{"name":"alert1", "expr":"ETH>CAKE"}' https://localhost:8008/crypto/alert
+
+curl -X GET -H "Content-Type: application/json" "https://api.terminaldweller.com/crypto/alert?key=alert10"
+curl -X DELETE -H "Content-Type: application/json" "https://api.terminaldweller.com/crypto/alert?key=alert10"
+curl -X POST -H "Content-Type: application/json" -d '{"name":"alert1", "expr":"ETH>CAKE"}' https://api.terminaldweller.com/crypto/alert
