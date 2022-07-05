27550c1f73edaaaca36294ec1939326009480c5b
terminaldweller <thabogre@gmail.com>
2022-07-05 11:47:25 +0430

    added goproxy env var to the docker images

--- test/endpoints.sh
+++ test/endpoints.sh
@@ -3,10 +3,10 @@ set -e
 set -x
 
 # sleep 5
-curl -k -X GET https://localhost:8008/crypto/price?name=CAKE&unit=USD
-curl -k -X GET https://localhost:8008/crypto/pair?one=ETH&two=CAKE&multiplier=4.0
+curl -k -X GET "https://localhost:8008/crypto/price?name=CAKE&unit=USD"
+curl -k -X GET "https://localhost:8008/crypto/pair?one=ETH&two=CAKE&multiplier=4.0"
 curl -k -X POST -H "Content-Type: application/json" -d '{"name":"alert1", "expr":"ETH>CAKE"}' https://localhost:8008/crypto/alert
 
-curl -X GET -H "Content-Type: application/json" "https://api.terminaldweller.com/crypto/alert?key=alert10"
+curl -X GET -H "Content-Type: application/json" "https://api.terminaldweller.com/crypto/alert?key=alert13"
 curl -X DELETE -H "Content-Type: application/json" "https://api.terminaldweller.com/crypto/alert?key=alert10"
 curl -X POST -H "Content-Type: application/json" -d '{"name":"alert1", "expr":"ETH>CAKE"}' https://api.terminaldweller.com/crypto/alert
